21aae9e1a4bfc4e5e8d7a85cf818751f17c917aa
Zoe Xanos <zxanos@gmail.com>
2020-05-08 13:51:02 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -18,10 +18,7 @@ def update_status
          {name: "Nurse", age: "44", attitude: "worried"}
          ]
       }
-      :additional_characters => [
-        {name: "Prince Escalus"}{name: "Apothecary"}
-      ]
-      }
+      
    }
 
   # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
@@ -64,7 +61,7 @@ def add_characters
    #   - The second hash should have the :name key set to "Apothecary" 
    # Code your solution here:
 
-
+add_characters
 
 
 
