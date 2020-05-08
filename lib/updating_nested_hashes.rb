85270fa2fd7f10ccf6ec7c135675b3601b5619fa
Zoe Xanos <zxanos@gmail.com>
2020-05-08 13:48:10 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -18,6 +18,10 @@ def update_status
          {name: "Nurse", age: "44", attitude: "worried"}
          ]
       }
+      :additional_characters => [
+        {name: "Prince Escalus"}{name: "Apothecary"}
+      ]
+      }
    }
 
   # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
